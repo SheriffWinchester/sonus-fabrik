5b6f9d60a696ee1756fc8b87dc614de52e63e296
SheriffKovalski <m3llpak@gmail.com>
2022-12-02 16:28:26 +0200

    BoundsCheck tweaks

--- Assets/Scripts/BlockClickMove.cs
+++ Assets/Scripts/BlockClickMove.cs
@@ -7,28 +7,29 @@ public class BlockClickMove : MonoBehaviour
     GameObject selectedObject;
     Vector3 offset;
 
-    [SerializeField] public bool isDragged;
+    public bool isDragged = true;
     void Update()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && isDragged == true)
         {
             Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
             //Debug.Log(targetObject);
+
             if (targetObject)
             {
                 selectedObject = targetObject.transform.gameObject;
                 offset = selectedObject.transform.position - mousePosition;
-                
-                isDragged = true;
+            
 
             }
+            
         }
         if (Input.GetMouseButtonUp(0) && selectedObject)
         {
             selectedObject = null;
-            isDragged = false;
         }
+       
         if (selectedObject) 
         {
             selectedObject.transform.position = mousePosition + offset;
--- Assets/Scripts/BoundsCheck.cs
+++ Assets/Scripts/BoundsCheck.cs
@@ -10,25 +10,39 @@ public class BoundsCheck : MonoBehaviour
     Collider2D factorySlotCollider;
     Vector3 test = new Vector3(3, 3, 3);
     BlockClickMove blockClickMove;
+    bool isIntersected;
+    Vector3 slotPosition;
 
     void Start() 
     {
             thisCollider = GetComponent<Collider2D>();
             factorySlotCollider = slotObject.GetComponent<Collider2D>();
+            blockClickMove = GetComponent<BlockClickMove>();
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Collision " + collision.gameObject.name);
-        if (collision.gameObject.tag == "FactorySlot")
+        isIntersected = true;
+        slotPosition = collision.transform.position;
+        // Debug.Log("Collision " + collision.gameObject.name);
+        // if (collision.gameObject.tag == "FactorySlot")
+        // {
+        //     blockClickMove.isDragged = false;
+        //     transform.position = collision.transform.position;
+        // }
+    }
+
+    void Update() {
+        if (isIntersected == true) 
         {
-            blockClickMove.isDragged = false;
-            transform.position = collision.transform.position;
+            transform.position = slotPosition;
         }
     }
+
     // void Update() {
     //     if (factorySlotCollider.bounds.Intersects(thisCollider.bounds)) 
     //     {
+    //         blockClickMove.isDragged = false;
     //         thisCollider.transform.position = factorySlotCollider.transform.position;
     //         Debug.Log(factorySlotCollider.name);
     //     }
